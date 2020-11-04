563acd8872b5b17b46ac33a2ea420436e2d97755
hisamatsu yoshiyuki <hisasann25@gmail.com>
2020-11-04 19:33:38 +0900

    fix: remove all comment for test

--- Formula/ide.rb
+++ Formula/ide.rb
@@ -6,32 +6,13 @@ class Ide < Formula
   homepage "https://github.com/hisasann/ide"
   url "https://github.com/hisasann/ide/releases/download/v0.0.1/ide"
   sha256 "91ee41935651db8ba4d02892b655744b26c9b9ebe39c849e92b135770b651f83"
-  license ""
-
-  # depends_on "cmake" => :build
+  license "MIT"
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-    # Remove unrecognized options if warned by configure
-    #system "./configure", "--disable-debug",
-    #                      "--disable-dependency-tracking",
-    #                      "--disable-silent-rules",
-    #                      "--prefix=#{prefix}"
-    # system "cmake", ".", *std_cmake_args
-    # 実行ファイルを実行Pathに配置するよの意味
     bin.install "ide"
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test ide`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
     system "true"
   end
 end
