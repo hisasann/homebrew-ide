7235edaa906bc8796f3edba7c418fb79af721268
hisamatsu yoshiyuki <hisasann25@gmail.com>
2020-11-04 19:52:09 +0900

    fix: remove header comment for test

--- Formula/ide.rb
+++ Formula/ide.rb
@@ -1,6 +1,3 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Ide < Formula
   desc "Hisasann ide"
   homepage "https://github.com/hisasann/ide"
