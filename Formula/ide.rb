5ba6b30ec373d4cc64039a5ab03747884696b121
hisamatsu yoshiyuki <hisasann25@gmail.com>
2021-06-22 21:44:59 +0900

    feat: update sha256

--- Formula/ide.rb
+++ Formula/ide.rb
@@ -2,7 +2,7 @@ class Ide < Formula
   desc "Hisasann ide"
   homepage "https://github.com/hisasann/ide"
   url "https://github.com/hisasann/ide/releases/download/v0.0.2/ide"
-  sha256 "13b1bb18d9fc380daa057628c343337eb3b590184af0a1823e2649eeffe34645"
+  sha256 "ea597c68cd2f8563afb7736124617cbee709fa1af74f934c762e148886f373b9"
   license "MIT"
 
   def install
